c5e9e4f90876e5a1043712efd45e88551cf68150
Chroma-YT <189774105+Chroma-YT@users.noreply.github.com>
2024-11-30 15:38:04 -0500

    Update scripts.js

--- Tinkercraft/scripts.js
+++ Tinkercraft/scripts.js
@@ -1,16 +1,17 @@
 document.querySelectorAll('.grid-item').forEach(item => {
+    let hoverTimeout;
+
     item.addEventListener('mouseover', () => {
         item.style.backgroundColor = 'darkgray';
-    });
-    item.addEventListener('mouseout', () => {
-        item.style.backgroundColor = 'lightgray';
-    });
-    item.addEventListener('mouseenter', () => {
-        setTimeout(() => {
+        hoverTimeout = setTimeout(() => {
             document.getElementById('sidebar-title').innerText = item.dataset.title;
             document.getElementById('sidebar-image').src = item.dataset.image;
             document.getElementById('sidebar-text').innerText = item.dataset.text;
         }, 200);
     });
-});
 
+    item.addEventListener('mouseout', () => {
+        item.style.backgroundColor = 'lightgray';
+        clearTimeout(hoverTimeout);
+    });
+});
