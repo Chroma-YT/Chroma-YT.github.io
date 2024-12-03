666749fb93882585da3aeba7695a747cbbe5ba7b
Chroma-YT <189774105+Chroma-YT@users.noreply.github.com>
2024-12-03 08:26:14 -0500

    added image slider (maybe)

--- Tinkercraft/script.js
+++ Tinkercraft/script.js
@@ -50,3 +50,76 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 });
+
+//Image slider
+function initComparisons() {
+    var x, i;
+    /* Find all elements with an "overlay" class: */
+    x = document.getElementsByClassName("img-comp-overlay");
+    for (i = 0; i < x.length; i++) {
+        /* Once for each "overlay" element: pass the "overlay" element as a parameter when executing the compareImages function: */
+        compareImages(x[i]);
+    }
+}
+
+function compareImages(img) {
+    var slider, img, clicked = 0, w, h;
+    /* Get the width and height of the img element */
+    w = img.offsetWidth;
+    h = img.offsetHeight;
+    /* Set the width of the img element to 50%: */
+    img.style.width = (w / 2) + "px";
+    /* Create slider: */
+    slider = document.createElement("DIV");
+    slider.setAttribute("class", "img-comp-slider");
+    /* Insert slider */
+    img.parentElement.insertBefore(slider, img);
+    /* Position the slider in the middle: */
+    slider.style.top = (h / 2) - (slider.offsetHeight / 2) + "px";
+    slider.style.left = (w / 2) - (slider.offsetWidth / 2) + "px";
+    /* Execute a function when the mouse button is pressed: */
+    slider.addEventListener("mousedown", slideReady);
+    /* And another function when the mouse button is released: */
+    window.addEventListener("mouseup", slideFinish);
+    /* Or touched (for touch screens: */
+    slider.addEventListener("touchstart", slideReady);
+    /* And released (for touch screens: */
+    window.addEventListener("touchend", slideFinish);
+}
+
+function slideReady(e) {
+    /* Prevent any other actions that may occur when moving over the image: */
+    e.preventDefault();
+    /* The slider is now clicked and ready to move: */
+    clicked = 1;
+    /* Execute a function when the slider is moved: */
+    window.addEventListener("mousemove", slideMove);
+    window.addEventListener("touchmove", slideMove);
+}
+
+function slideFinish() {
+    /* The slider is no longer clicked: */
+    clicked = 0;
+}
+
+function slideMove(e) {
+    var pos;
+    /* If the slider is no longer clicked, exit this function: */
+    if (clicked == 0) return false;
+    /* Get the cursor's x position: */
+    pos = getCursorPos(e);
+    /* Prevent the slider from being positioned outside the image: */
+    if (pos < 0) pos = 0;
+    if (pos > img.offsetWidth) pos = img.offsetWidth;
+    /* Execute a function that will resize the overlay image according to the cursor: */
+    img.style.width = pos + "px";
+}
+
+function getCursorPos(e) {
+    var a, x = 0;
+    e = e || window.event;
+    a = e.pageX || e.touches[0].pageX;
+    return a;
+}
+
+initComparisons();
